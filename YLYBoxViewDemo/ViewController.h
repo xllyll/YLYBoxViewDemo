8f24fe694b810364ba9e38041b09424eabe545a6
xllyll_60 <yangliyin@outlook.com>
2015-03-12 15:47:29 +0800

    Initial Commit

---
+++ YLYBoxViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YLYBoxViewDemo
+//
+//  Created by xinyu_mac on 15/3/12.
+//  Copyright (c) 2015年 xllyll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YLYBoxViewDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YLYBoxViewDemo
+//
+//  Created by xinyu_mac on 15/3/12.
+//  Copyright (c) 2015年 xllyll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
